7d86ff84120d6278b32fb70bc3686a0004f20e69
Mahendra Singamsetty <mahendrasv@gmail.com>
2020-03-09 06:04:21 +0530

    Updated Todos

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -7,7 +7,7 @@ class Todo extends Component {
     todos: [
       {
         text: 'Learning React',
-        completed: false
+        completed: true
       },
       {
         text: 'Practicing React',
@@ -39,18 +39,57 @@ class Todo extends Component {
     });
   };
 
+  changeCompleted = (index) => {
+    let todos = this.state.todos;
+    todos[index].completed = !todos[index].completed;
+    this.setState({
+      todos: todos
+    });
+  };
+
+  getCompletedTodoCount = () => {
+    return this.state.todos.filter(todo => todo.completed === true).length;
+  };
+
+  deleteTodo = (index) => {
+    let todos = this.state.todos;
+    todos = todos.filter((todo, i) => i !== index );
+    this.setState({
+      todos: todos
+    });
+  };
+
   render() {
+
+    let todoLi;
+
+    todoLi = this.state.todos.map((todo, index) => {
+      let checked = todo.completed ? {'checked': 'true'} : {};
+      let completedStyle = todo.completed ? {color: 'lightgray', backgroundColor: 'yellow'} : {};
+      return (
+        <li>
+          <input type="checkbox" {...checked} onClick={() => this.changeCompleted(index)} />
+          <span style={completedStyle}>{todo.text}</span>
+          <button className="btn btn-danger" onClick={() => this.deleteTodo(index)}>x</button>
+        </li>
+      )
+    });
+
     return (
       <div>
         <h2>Todo Application</h2>
+        <div>
+          Total: <b>{this.state.todos.length}</b>
+        </div>
+        <div>
+          Completed: <b>{this.getCompletedTodoCount()}</b>
+        </div>
         New Todo text: <input type="text" value={this.state.newTodoText} onChange={this.changeText} />
         <button className="btn btn-success" onClick={this.addTodo}>Add</button>
         <br />
         <br />
         <ul>
-          {
-            this.state.todos.map(todo => <li>{todo.text}</li>)
-          }
+          {todoLi}
         </ul>
       </div>
     );
